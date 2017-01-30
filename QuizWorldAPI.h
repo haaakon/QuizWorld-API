e5bfc7e1bc1b87eaf56097e7cd5eea32edd49ba8
Håkon Bogen <bogen@beiningbogen.no>
2017-01-30 16:20:05 +0100

    no message

---
+++ QuizWorldAPI.h
@@ -0,0 +1,13 @@
+//
+//  QuizWorldAPI.h
+//  QuizWorldAPI
+//
+//  Created by Håkon Bogen on 29/01/17.
+//  Copyright © 2017 beiningbogen.no. All rights reserved.
+//
+
+#ifndef QuizWorldAPI_h
+#define QuizWorldAPI_h
+
+
+#endif /* QuizWorldAPI_h */
